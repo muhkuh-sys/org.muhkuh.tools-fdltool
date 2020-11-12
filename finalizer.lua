38dca0ce574f7d01fe029fd0da1a3cc75de69252
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-11-13 00:22:57 +0100

    Move the demo scripts to the local folder.

--- finalizer.lua
+++ finalizer.lua
@@ -9,6 +9,12 @@ t:install{
   ['local/fdltool.lua']                                    = '${install_base}/',
   ['local/fdl_to_wfp_template.lua']                        = '${install_base}/',
 
+  -- Add the demo files.
+  ['local/demo/FDL_NXHX90-JTAG_7833000r3_UseCaseC.fdl']    = '${install_base}/demo/',
+  ['local/demo/patch_mac_and_oem.json']                    = '${install_base}/demo/',
+  ['local/demo/patch_mac.json']                            = '${install_base}/demo/',
+  ['local/demo/patch_oem.json']                            = '${install_base}/demo/',
+
   ['${report_path}']                                       = '${install_base}/.jonchki/'
 }
 
